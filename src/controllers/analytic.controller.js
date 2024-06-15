deb9cd9ebc2e8d2e2b896618aace7c2165235912
Gyanesh Rao <gyaneshrao28@gmail.com>
2024-06-15 11:08:55 +0530

    v-2

--- src/controllers/analytic.controller.js
+++ src/controllers/analytic.controller.js
@@ -11,42 +11,25 @@ const getAnalyticData = async(req, res)=>{
 
 const getFilterAnalyticData = async (req, res) => {
     try {
-        const { endYear, topics, sectors, regions, pestles, sources, countries, cities } = req.query;
+        const { endYear, topics, sector, region, pestle, source, country, city } = req.query;
         const filters = {};
 
-        if (endYear) {
-            filters.end_year = Array.isArray(endYear) ? { $in: endYear } : endYear;
-        }
+        if (endYear) filters.end_year = endYear;
 
-        if (topics) {
+        if (topics !== undefined && topics !== '') {
             filters.topic = Array.isArray(topics) ? { $in: topics } : topics;
         }
 
-        if (sectors) {
-            filters.sector = Array.isArray(sectors) ? { $in: sectors } : sectors;
-        }
-
-        if (regions) {
-            filters.region = Array.isArray(regions) ? { $in: regions } : regions;
-        }
-
-        if (pestles) {
-            filters.pestle = Array.isArray(pestles) ? { $in: pestles } : pestles;
-        }
-
-        if (sources) {
-            filters.source = Array.isArray(sources) ? { $in: sources } : sources;
-        }
-
-        if (countries) {
-            filters.country = Array.isArray(countries) ? { $in: countries } : countries;
-        }
-
-        if (cities) {
-            filters.city = Array.isArray(cities) ? { $in: cities } : cities;
-        }
+        if (sector) filters.sector = sector;
+        if (region) filters.region = region;
+        if (pestle) filters.pestle = pestle;
+        if (source) filters.source = source;
+        if (country) filters.country = country;
+        if (city) filters.city = city;
 
         const data = await Analytic.find(filters);
+
+        
         res.json({ data });
     } catch (error) {
         console.error('FILTER ERROR', error.message);
